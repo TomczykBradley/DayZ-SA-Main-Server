4484473a5e6b2e2cda864482c3d496af50b5c250
TomczykBradley <49622986+TomczykBradley@users.noreply.github.com>
2019-05-01 01:57:07 -0400

    Patch Goals and run.bat
    Added Some patch update notes & removed Unlimited Sprint

--- Main Directory Files/0.1/run.bat
+++ Main Directory Files/0.1/run.bat
@@ -1,1 +1,1 @@
-start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=serverLogs -mod=@CF;@Trader;@Community-Framework;@Community-Online-Tools;@VanillaPlusPlusMap;@Mass'sManyItemOverhaul;@Unlimited Stamina;
+start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=serverLogs -mod=@CF;@Trader;@Community-Framework;@Community-Online-Tools;@VanillaPlusPlusMap;@Mass'sManyItemOverhaul;
