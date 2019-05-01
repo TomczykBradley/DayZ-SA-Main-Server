f4aa7c128a8b925bd87837aaff6e60283379a7f1
TomczykBradley <49622986+TomczykBradley@users.noreply.github.com>
2019-05-01 03:37:07 -0400

    Added Mods
    Added All 4 mods to run.bat
    Added MosinScope type in type xml file

--- Main Directory Files/0.2/run.bat
+++ Main Directory Files/0.2/run.bat
@@ -1,1 +1,1 @@
-start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=serverLogs -mod=@CF;@Trader;@Community-Framework;@Community-Online-Tools;@VanillaPlusPlusMap;@Mass'sManyItemOverhaul;
+start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -noFilePatching -profiles=serverLogs -mod=@CF;@Trader;@Community-Framework;@Community-Online-Tools;@VanillaPlusPlusMap;@Mass'sManyItemOverhaul;@Sullen-Skies-for-Chernarus-Plus;@dbo_surfaces;@Increased-Lumens;@MosinScope;
